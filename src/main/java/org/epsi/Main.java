c4114f35af7215451e32636c082b2a7fc521e4e2
SylvainDuret <sylduret@hotmail.fr>
2021-03-05 13:16:18 +0100

    first commit

---
+++ src/main/java/org/epsi/Main.java
@@ -0,0 +1,8 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+       
+    }
+}
---
+++ src/main/java/org/epsi/depend.java
@@ -0,0 +1,9 @@
+import org.springframework.stereotype.Component;
+
+@Component
+public class depend {
+    int test=4;
+    public void main() {
+
+    }
+}
